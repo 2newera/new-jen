e716a5f7cf656ece49ee8aead92cd764bbb174fc
max <absj@gmail.com>
2022-11-05 19:39:13 +0530

    new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{  
     public static void main(String args[]){  
-     System.out.println("Hello World Jenkins new");  
+     System.out.println("Hello World Jenkins 2 step new");  
     }  
 }  
